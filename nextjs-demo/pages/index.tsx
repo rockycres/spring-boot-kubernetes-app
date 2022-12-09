43406caab7a76a1635481ceac36edc12b1e3e14c
rockycres <98057055+rockycres@users.noreply.github.com>
2022-12-09 13:57:51 +1100

    UI Changes

---
+++ nextjs-demo/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import {NextPage} from "next";
+
+
+const Home : NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Welcoem Santhosh</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ reactjs-demo/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Home from "./Home";
+import About from "./About";
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+function App() {
+  return (
+
+      <BrowserRouter>
+          <nav>
+              <Link to="/">Home</Link> | {" "}
+              <Link to="about">About</Link>
+          </nav>
+          <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+
+          </Routes>
+
+
+
+      </BrowserRouter>
+  );
+}
+
+export default App;
+
+
